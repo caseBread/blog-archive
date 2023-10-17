b9158b085d015c0f20e8225718267f1d1b75f770
front_김건우 <kgu0515@gmail.com>
2023-10-17 23:19:16 +0900

    feat : delete modified darkmode button

--- src/app.feature/header/menu/index.tsx
+++ src/app.feature/header/menu/index.tsx
@@ -4,14 +4,6 @@ import Link from 'next/link';
 const HeaderMenu = () => {
   return (
     <div className="flex gap-8">
-      <button>
-        <Image
-          src="/icon/ic-darkmode-light.svg"
-          alt="다크모드-전환"
-          width="20"
-          height="20"
-        />
-      </button>
       <Link href="/posts/about.mdx">about</Link>
       <Link href="/posts">posts</Link>
     </div>
